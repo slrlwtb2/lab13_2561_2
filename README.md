52b87a893d997041847d429402f56fda672b8662
lab314-11@labeng.local <lab314-11@labeng.local>
2019-02-26 17:58:01 +0700

    ยาก

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# NAME:
-# STUDENT ID:
+# NAME: ธนาสิริ เปรมัษเฐียร
+# STUDENT ID: 610610697
--- lab13_1.cpp
+++ lab13_1.cpp
@@ -12,9 +12,32 @@ void swap(T d[],int x,int y){
 template <typename T>
 void insertionSort(T d[],int N){
 	
-//Write your code here.
 
+	int start, i, j=0;
+	for(i=1; i<N; i++){
+		start = d[i];
+		j = i-1;
+		for(int k = 0; k < i; k++) 
+		cout << d[k] << " ";
+        cout<<"["<<d[i]<<"]";
+        for(int t = i+1; t < N; t++) 
+		cout << d[t] << " ";
+        cout<<"==>";
+		while( j>=0 && d[j] < start){
+			swap(d,j+1,j);
+			j = j-1;
+	}
+	d[j+1] = start;
+	for(int k = 0; k <= j; k++) 
+	cout << d[k] << " ";
+    cout<<"["<<d[j+1]<<"]";
+    for(int u = j+2; u < N; u++) 
+	cout << d[u] << " ";
+	cout<<"\n";
+	}
 }
+
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
